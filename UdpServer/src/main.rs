0a94899f9e45bb982a6aa655944b09d5c2242ae2
Daniel Elharar <88079730+MaSsTerKidd0@users.noreply.github.com>
2023-09-07 19:09:47 +0300

    udp server trying to initialize

--- UdpServer/src/main.rs
+++ UdpServer/src/main.rs
@@ -1,3 +1,28 @@
+use std::net::UdpSocket;
+
+pub trait ServerInterface
+{
+    fn executeServer();
+}
+
+impl ServerInterface for Server {
+
+
+    fn executeServer() {
+        todo!()
+    }
+}
+pub trait Server<Protocol>
+{
+    fn start(&self);
+    fn send(&self, protocol: Protocol )
+    {
+
+    }
+    fn close(&self);
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
